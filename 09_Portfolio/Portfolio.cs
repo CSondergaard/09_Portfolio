1ef9a8c15055a80f8b307ee6addca9345ce3dfdf
casp4 <casp4@LAPTOP-O5C9AQD0>
2016-09-26 12:34:41 +0200

    Add project files.

---
+++ 09_Portfolio/Portfolio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09_Portfolio
+{
+    internal class Portfolio
+    {
+        List<IAsset> assetlist = new List<IAsset>();
+        public Portfolio()
+        {
+            assetlist = new List<IAsset>();
+        }
+
+        public Portfolio(List<IAsset> list)
+        {
+            assetlist = list;
+        }
+        public void AddAsset(IAsset item)
+        {
+            assetlist.Add(item);
+        }
+
+        public double GetTotalValue()
+        {
+            double value = 0;
+            foreach (IAsset item in assetlist)
+            {
+                value += item.GetValue();
+            }
+            return value;
+        }
+    }
+}
---
+++ 09_Portfolio/Stock.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace _09_Portfolio
+{
+    interface IAsset
+    {
+        double GetValue();
+    }
+
+    internal class Stock : IAsset
+    {
+
+        public string Symbol { get; set; }
+        public double PricePerShare { get; set; }
+        public int NumShares { get; set; }
+
+        public Stock()
+        {
+
+        }
+        public Stock(string s, double price, int shares)
+        {
+            Symbol = s;
+            PricePerShare = price;
+            NumShares = shares;
+        }
+
+        public override string ToString()
+        {
+            return "Stock[symbol=" + Symbol + ",pricePerShare=" + PricePerShare.ToString().Replace(',', '.') + ",numShares=" + NumShares + "]";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ToString() != obj.ToString())
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public double GetValue()
+        {
+            double i = NumShares * PricePerShare;
+            return i;
+        }
+
+    
+        public static double TotalValue(IAsset[] sar)
+        {
+            double value = 0;
+            foreach (var item in sar)
+            {
+                value = value + item.GetValue();
+            }
+            return value;
+        }
+
+
+    } // Stock end
+
+    internal class SavingsAccount : IAsset
+    {
+        public string AccountName { get; set; }
+        public double value { get; set; }
+        public double InterestRate { get; set; }
+        public SavingsAccount(string s, double i, double d)
+        {
+            AccountName = s;
+            value = i;
+            InterestRate = d;
+        }
+
+        public void ApplyInterest()
+        {
+            double rate = value * (InterestRate / 100);
+            value = value + rate;
+            
+            
+        }
+
+        public double GetValue()
+        {
+            return value;
+        }
+
+        public override string ToString()
+        {
+            return "SavingsAccount[value=" + value + ".0,interestRate=" + InterestRate.ToString().Replace(',', '.') + "]";
+        }       
+
+    }
+}
